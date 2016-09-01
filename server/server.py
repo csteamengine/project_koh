e40e25e51b1929b2c37a6c55525e9f8abb7ba2d7
bcannon <bcannon@iastate.edu>
2016-09-01 14:04:13 -0500

    Restructured files

--- server/server.py
+++ server/server.py
@@ -1,7 +1,7 @@
-import SocketServer
+import socketserver
 
 
-class MyTCPHandler(SocketServer.BaseRequestHandler):
+class MyTCPHandler(socketserver.BaseRequestHandler):
     """
     The RequestHandler class for our server.
 
@@ -22,7 +22,7 @@ if __name__ == "__main__":
     HOST, PORT = "localhost", 8000
 
     # Create the server, binding to localhost on port 8000
-    server = SocketServer.TCPServer((HOST, PORT), MyTCPHandler)
+    server = socketserver.TCPServer((HOST, PORT), MyTCPHandler)
 
     # Activate the server; this will keep running until you
     # interrupt the program with Ctrl-C

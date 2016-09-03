c98c711bba427fbc4fbb01e052dd05b7630138f5
csteamengine <gcs@iastate.edu>
2016-09-02 23:41:47 -0500

    File uploads are done and successful

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -7,6 +7,15 @@
 ## Download Python
   - [Download](https://www.python.org/downloads/)
   - Make sure to install Python 3.5.2!
+## Install tornado python API
+  - [Install](http://www.tornadoweb.org/en/stable/index.html)
+  - Make sure to install the python3 version of tornado
+  - The following will install tornado with python3 on linux and mac (May have to install pip first)
+  ```
+  sudo python3 -m pip install tornado
+  ```
+  - on windows, may have to follow the manual installation on the site.
+
 ## Set Up Python Server
   - Navigate to File | Settings | Project: se329_project_1 | Project Interpreter
   - Click to create a new local project interpreter direct it to the python 3.5 download location
@@ -16,12 +25,16 @@
 ## Set up Server 
   - Navigate to Run | Edit Configurations
   - Click the '+' icon in the top right and select 'Python'
+  - Point the script to our server script 'server/server.py'
   - Make sure the interpreter is pointing to the python3.5 interpreter we created in the previous step
   - Name the server something. (Ex// Server 8000)
 ## Launch 
   - To launch the server, click the Play button in the top right
   - Navigate to localhost:8000 in browser
+  
+## Contributing
+  - When you change anything, you will need to restart the server by clicking the restart button
+  - Then you can go to the browser and reload the page.
+  
 
-# If we can't get a python server to work
 
-## We will have to figure something else out.
---
+++ server/server.py
@@ -0,0 +1,62 @@
+import tornado.ioloop
+import tornado.web
+import tornado.websocket
+import os, uuid
+
+from tornado.options import define, options, parse_command_line
+
+define("port", default=8888, help="run on the given port", type=int)
+__UPLOADS__ = "upload/"
+# we gonna store clients in dictionary..
+clients = dict()
+
+
+class IndexHandler(tornado.web.RequestHandler):
+    @tornado.web.asynchronous
+    def get(self):
+        self.render("../client/index.html")
+
+
+class Upload(tornado.web.RequestHandler):
+    def post(self):
+        fileinfo = self.request.files['filearg'][0]
+        print("fileinfo is", fileinfo)
+        fname = fileinfo['filename']
+        extn = os.path.splitext(fname)[1]
+        cname = str(uuid.uuid4()) + extn
+        fh = open(__UPLOADS__ + cname, 'wb')
+        fh.write(fileinfo['body'])
+        self.render("../client/index.html")
+        # self.finish(cname + " is uploaded!! Check %s folder" %__UPLOADS__)
+
+
+class WebSocketHandler(tornado.websocket.WebSocketHandler):
+    def open(self, *args):
+        self.id = self.get_argument("Id")
+        self.stream.set_nodelay(True)
+        clients[self.id] = {"id": self.id, "object": self}
+
+    def on_message(self, message):
+        """
+        when we receive some message we want some message handler..
+        for this example i will just print message to console
+        """
+        print("Client %s received a message : %s" % (self.id, message))
+        self.write_message("Hello Client!")
+
+    def on_close(self):
+        if self.id in clients:
+            del clients[self.id]
+
+
+# This part is the key to success. We have to redirect the websocket calls to a different URL ex//: /websocket
+app = tornado.web.Application([
+    (r'/', IndexHandler),
+    (r'/websocket', WebSocketHandler),
+    (r'/upload', Upload)
+])
+
+if __name__ == '__main__':
+    parse_command_line()
+    app.listen(options.port)
+    tornado.ioloop.IOLoop.instance().start()

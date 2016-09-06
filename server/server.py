0cc2a4aaa94213892eb3b2e9757eed2a0c566571
bcannon <bcannon@iastate.edu>
2016-09-06 12:11:27 -0500

    Merging code with master

--- server/server.py
+++ server/server.py
@@ -1,23 +1,17 @@
 import socketserver
 
 
-class MyTCPHandler(socketserver.BaseRequestHandler):
-    """
-    The RequestHandler class for our server.
-
-    It is instantiated once per connection to the server, and must
-    override the handle() method to implement communication to the
-    client.
-    """
+class MyTCPHandler(socketserver.StreamRequestHandler):
 
     def handle(self):
-        # self.request is the TCP socket connected to the client
-        self.data = self.request.recv(1024).strip()
+        # self.rfile is a file-like object created by the handler;
+        # we can now use e.g. readline() instead of raw recv() calls
+        self.data = self.rfile.readline().strip()
+        print("{} wrote:".format(self.client_address[0]))
         print(self.data)
-        # just send back the same data, but upper-cased
-        # self.request.sendall(self.data.upper())
-        message = "Hello From the other side!!!"
-        self.request.send(message.encode())
+        # Likewise, self.wfile is a file-like object used to write back
+        # to the client
+        self.wfile.write(self.data.upper())
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8000
